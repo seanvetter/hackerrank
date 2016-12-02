4907fdde0715119a8ab8e6cd5a8f146488041e9e
seanvetter <vetters10@gmail.com>
2016-12-01 16:20:58 -0800

    Day 12

---
+++ day12/inheritance.py
@@ -0,0 +1,43 @@
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, fname, lname, idNumber, scores):
+        super().__init__(fname, lname, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        total = 0
+        for n in self.scores:
+            total += n
+        avg = total/len(self.scores)
+        if avg >= 90:
+            return 'O'
+        elif avg >= 80:
+            return 'E'
+        elif avg >= 70:
+            return 'A'
+        elif avg >= 55:
+            return 'P'
+        elif avg >= 40:
+            return 'D'
+        else: # avg >= 40:
+            return 'T'
+
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
